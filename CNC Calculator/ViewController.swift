23112aca3028e5bdabf409b37d813761972d3b61
Alex de la Rosa <alexdelarosacortes@gmail.com>
2016-03-16 13:22:52 -0600

    Formulas agregadas

--- CNC Calculator/ViewController.swift
+++ CNC Calculator/ViewController.swift
@@ -34,6 +34,22 @@ class ViewController: UIViewController {
     }
    
     @IBAction func Calcular() {
+        if tfPuntox.text != nil && tfPuntoy.text != nil && tfRadio.text != nil && tfAngulo.text != nil {
+            let puntoX = Double(tfPuntox.text!)!
+            let puntoY = Double(tfPuntoy.text!)!
+            let radio = Double(tfRadio.text!)!
+            let angulo = Double(tfAngulo.text!)! * M_PI / 180.0
+            let formula1 = radio * cos(angulo)
+            let formula2 = radio - formula1
+            let formula3 = formula2 / tan(angulo)
+            let formula4 = radio * sin(angulo) - formula3
+            
+            lbFormula1.text = String(format: "F1 = %0.2f", formula1)
+            lbFormula2.text = String(format: "F2 = %0.2f", formula2)
+            lbFormula3.text = String(format: "F3 = %0.2f", formula3)
+            lbFormula4.text = String(format: "F4 = %0.2f", formula4)
+            
+        }
     }
     
     override func viewDidLoad() {
