d3cd2f74e9d0e3b876ed57912c9bd5fe7860c2cf
Pablo Diaz Mateusgit config --global user.email “pabloso92@yahoo.com <pabloso92@yahoo.com>
2016-03-16 12:39:40 -0600

    Storyboard

--- CNC Calculator/ViewController.swift
+++ CNC Calculator/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scPosicion: UISegmentedControl!
+    @IBOutlet weak var scCuadrante: UISegmentedControl!
+    @IBOutlet weak var imImagen: UIImageView!
+    @IBOutlet weak var tfPuntox: UITextField!
+    @IBOutlet weak var tfPuntoy: UITextField!
+    @IBOutlet weak var tfRadio: UITextField!
+    @IBOutlet weak var tfAngulo: UITextField!
+    @IBOutlet weak var lbPunto1: UILabel!
+    @IBOutlet weak var lbPunto2: UILabel!
+    @IBOutlet weak var lbFormula1: UILabel!
+    @IBOutlet weak var lbFormula2: UILabel!
+    @IBOutlet weak var lbFormula3: UILabel!
+    @IBOutlet weak var lbFormula4: UILabel!
+    
+    
+    @IBAction func CambioPosicion(sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func CambioCuadrante(sender: UISegmentedControl) {
+        
+    }
+   
+    @IBAction func Calcular() {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
