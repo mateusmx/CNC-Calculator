09a91ef0e25d0659899c470bcfbb3a2c7f73c5f3
alex de la Rosa <alexdelarosacortes@gmail.com>
2016-03-31 07:41:07 -0600

    Validate invalid characters on textfields

--- CNC Calculator/ViewController.swift
+++ CNC Calculator/ViewController.swift
@@ -76,94 +76,99 @@ class ViewController: UIViewController {
     @IBAction func Calcular() {
         if tfPuntox.text != "" && tfPuntoy.text != "" && tfRadio.text != "" && tfAngulo.text != "" {
             
+            let puntoX = Double(tfPuntox.text!)
+            let puntoY = Double(tfPuntoy.text!)
+            let radio = Double(tfRadio.text!)
+            var angulo = Double(tfAngulo.text!)
+            
+            if puntoX != nil && puntoY != nil && radio != nil && angulo != nil {
+                
+                angulo = angulo! * M_PI / 180.0
+                
+                let formula1 = radio! * cos(angulo!)
+                let formula2 = radio! - formula1
+                let formula3 = formula2 / tan(angulo!)
+                let formula4 = radio! * sin(angulo!) - formula3
+                
+                lbFormula1.text = String(format: "F1 = %0.2f", formula1)
+                lbFormula2.text = String(format: "F2 = %0.2f", formula2)
+                lbFormula3.text = String(format: "F3 = %0.2f", formula3)
+                lbFormula4.text = String(format: "F4 = %0.2f", formula4)
+                
+                var dpunto1X: Double!
+                var dpunto1Y: Double!
+                var dpunto2X: Double!
+                var dpunto2Y: Double!
+                
+                // Posición Horizontal
+                if self.indexPosicion == 0 {
+                    switch self.indexCuadrante {
+                    // Primer Cuadrante
+                    case 0:
+                        dpunto2X = puntoX! - formula4
+                        dpunto2Y = puntoY!
+                        dpunto1X = puntoX! + formula3
+                        dpunto1Y = puntoY! - formula2
+                        
+                    // Segundo Cuadrante
+                    case 1:
+                        dpunto2X = puntoX! + formula4
+                        dpunto2Y = puntoY!
+                        dpunto1X = puntoX! - formula3
+                        dpunto1Y = puntoY! - formula2
+                        
+                    // Tercer Cuadrante
+                    case 2:
+                        dpunto1X = puntoX! - formula3
+                        dpunto1Y = puntoY! + formula2
+                        dpunto2X = puntoX! + formula4
+                        dpunto2Y = puntoY!
+                        
+                    // Cuarto Cuadrante
+                    default:
+                        dpunto1X = puntoX! + formula3
+                        dpunto1Y = puntoY! + formula2
+                        dpunto2X = puntoX! - formula4
+                        dpunto2Y = puntoY!
+                    }
+                }
+                    // Posición Vertical
+                else {
+                    switch self.indexCuadrante {
+                    // Primer Cuadrante
+                    case 0:
+                        dpunto1X = puntoX! - formula2
+                        dpunto1Y = puntoY! + formula3
+                        dpunto2X = puntoX!
+                        dpunto2Y = puntoY! - formula4
+                        
+                    // Segundo Cuadrante
+                    case 1:
+                        dpunto1X = puntoX! + formula2
+                        dpunto1Y = puntoY! + formula3
+                        dpunto2X = puntoX!
+                        dpunto2Y = puntoY! - formula4
+                        
+                    // Tercer Cuadrante
+                    case 2:
+                        dpunto2X = puntoX!
+                        dpunto2Y = puntoY! + formula4
+                        dpunto1X = puntoX! + formula2
+                        dpunto1Y = puntoY! - formula3
+                        
+                    // Cuarto Cuadrante
+                    default:
+                        dpunto2X = puntoX!
+                        dpunto2Y = puntoY! + formula4
+                        dpunto1X = puntoX! - formula2
+                        dpunto1Y = puntoY! - formula3
+                    }
+                }
+                
+                lbPunto1.text = String(format: "P1 = (%0.2f, %0.2f)", dpunto1X, dpunto1Y)
+                lbPunto2.text = String(format: "P2 = (%0.2f, %0.2f)", dpunto2X, dpunto2Y)
+            }
             self.view.endEditing(true)
-            
-            let puntoX = Double(tfPuntox.text!)!
-            let puntoY = Double(tfPuntoy.text!)!
-            let radio = Double(tfRadio.text!)!
-            let angulo = Double(tfAngulo.text!)! * M_PI / 180.0
-            let formula1 = radio * cos(angulo)
-            let formula2 = radio - formula1
-            let formula3 = formula2 / tan(angulo)
-            let formula4 = radio * sin(angulo) - formula3
-            
-            lbFormula1.text = String(format: "F1 = %0.2f", formula1)
-            lbFormula2.text = String(format: "F2 = %0.2f", formula2)
-            lbFormula3.text = String(format: "F3 = %0.2f", formula3)
-            lbFormula4.text = String(format: "F4 = %0.2f", formula4)
-            
-            var dpunto1X: Double!
-            var dpunto1Y: Double!
-            var dpunto2X: Double!
-            var dpunto2Y: Double!
-            
-            // Posición Horizontal
-            if self.indexPosicion == 0 {
-                switch self.indexCuadrante {
-                    // Primer Cuadrante
-                case 0:
-                    dpunto2X = puntoX - formula4
-                    dpunto2Y = puntoY
-                    dpunto1X = puntoX + formula3
-                    dpunto1Y = puntoY - formula2
-                    
-                    // Segundo Cuadrante
-                case 1:
-                    dpunto2X = puntoX + formula4
-                    dpunto2Y = puntoY
-                    dpunto1X = puntoX - formula3
-                    dpunto1Y = puntoY - formula2
-                    
-                    // Tercer Cuadrante
-                case 2:
-                    dpunto1X = puntoX - formula3
-                    dpunto1Y = puntoY + formula2
-                    dpunto2X = puntoX + formula4
-                    dpunto2Y = puntoY
-                    
-                    // Cuarto Cuadrante
-                default:
-                    dpunto1X = puntoX + formula3
-                    dpunto1Y = puntoY + formula2
-                    dpunto2X = puntoX - formula4
-                    dpunto2Y = puntoY
-                }
-            }
-                // Posición Vertical
-            else {
-                switch self.indexCuadrante {
-                    // Primer Cuadrante
-                case 0:
-                    dpunto1X = puntoX - formula2
-                    dpunto1Y = puntoY + formula3
-                    dpunto2X = puntoX
-                    dpunto2Y = puntoY - formula4
-                    
-                    // Segundo Cuadrante
-                case 1:
-                    dpunto1X = puntoX + formula2
-                    dpunto1Y = puntoY + formula3
-                    dpunto2X = puntoX
-                    dpunto2Y = puntoY - formula4
-                    
-                    // Tercer Cuadrante
-                case 2:
-                    dpunto2X = puntoX
-                    dpunto2Y = puntoY + formula4
-                    dpunto1X = puntoX + formula2
-                    dpunto1Y = puntoY - formula3
-                    
-                    // Cuarto Cuadrante
-                default:
-                    dpunto2X = puntoX
-                    dpunto2Y = puntoY + formula4
-                    dpunto1X = puntoX - formula2
-                    dpunto1Y = puntoY - formula3
-                }
-            }
-            
-            lbPunto1.text = String(format: "P1 = (%0.2f, %0.2f)", dpunto1X, dpunto1Y)
-            lbPunto2.text = String(format: "P2 = (%0.2f, %0.2f)", dpunto2X, dpunto2Y)
         }
     }
     
